23c6f59c51b5b5996b3479e5c52198e90244392a
EC2 Default User <ec2-user@ip-172-31-82-22.ec2.internal>
2018-12-12 19:11:02 +0000

    teste

--- server.js
+++ server.js
@@ -3,11 +3,36 @@ var express = require('express');
 
 var app = express();
 
+
+var mysql = require("mysql");
+//Database connection
+app.use(function(req, res, next){
+ 
+	res.locals.connection = mysql.createConnection({
+		host     : 'mydb.csv5qvzrti0z.us-east-1.rds.amazonaws.com',
+		user     : 'root',
+		password : '#192341!dev',
+		database : 'general'
+	});
+	res.locals.connection.connect();
+	next();
+});
+
+
 app.get('/', function(req, res) {
   res.send({
     "Output": "Hello World!"
   });
 });
+
+app.get('/user', function(req, res) {
+ res.locals.connection.query('SELECT * from members', function (error, results, fields) {
+		if(error) throw error;
+		res.send(JSON.stringify(results));
+	});
+ 
+});
+
 
 app.post('/', function(req, res) {
   res.send({
