f97e60f19cf7dbff218e8d8336eda0d45dc5c515
EC2 Default User <ec2-user@ip-172-31-82-22.ec2.internal>
2018-12-13 17:00:34 +0000

    cross

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express = require('express');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 var mysql = require("mysql");
 //Database connection
